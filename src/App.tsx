365af350873bc278912a15c95d737af834ef1d13
dongmyoungLee <pajang1515@daum.net>
2022-02-21 00:17:18 +0900

    0221

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-function App() {
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
-  return <div></div>
+function App() {
+  const [minute, setMinute] = useRecoilState(minuteState)
+  const hours = useRecoilValue(hourSelector)
+  const onMinuteChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setMinute(+event.currentTarget.value)
+  }
+
+  return (
+    <div>
+      <input value={minute} onChange={onMinuteChange} type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  )
 }
 
 
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,18 @@
 import { atom, selector } from "recoil";
+
+
+export const minuteState = atom({
+  key : "minutes",
+  default : 0
+})
+
+export const hourSelector = selector({
+  key : "hours",
+  
+  get : ({get}) => {
+    //get은 atom의 값을 가져오게끔 하는 함수이다.
+    const minutes = get(minuteState)
+    return minutes / 60 ;
+  }
+  // 어떤값을 return 하든 그 값은 hourSelector의 값으로들어감.
+})
