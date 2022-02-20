feb9ae1024620369a333e59954c0239a28419758
dongmyoungLee <pajang1515@daum.net>
2022-02-21 00:44:30 +0900

    ma

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minute, setMinute] = useRecoilState(minuteState)
-  const hours = useRecoilValue(hourSelector)
+  const [hours, setHours] = useRecoilState(hourSelector)
   const onMinuteChange = (event:React.FormEvent<HTMLInputElement>) => {
     setMinute(+event.currentTarget.value)
+  }
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value)
   }
 
   return (
     <div>
       <input value={minute} onChange={onMinuteChange} type="number" placeholder="Minutes" />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
     </div>
   )
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -6,13 +6,18 @@ export const minuteState = atom({
   default : 0
 })
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key : "hours",
   
   get : ({get}) => {
     //get은 atom의 값을 가져오게끔 하는 함수이다.
     const minutes = get(minuteState)
     return minutes / 60 ;
+    // 어떤값을 return 하든 그 값은 hourSelector의 값으로들어감.
+  },
+  set: ({set}, newValue) => {
+    const minutes = Number(newValue) * 60
+    set(minuteState, minutes)
   }
-  // 어떤값을 return 하든 그 값은 hourSelector의 값으로들어감.
+  
 })
